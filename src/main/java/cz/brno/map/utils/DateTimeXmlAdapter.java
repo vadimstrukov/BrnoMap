985421d4abee179f450e0e0943defc67f57ab817
vadimstrukov <vadim.strukov@gmail.com>
2016-06-30 16:40:53 +0200

    Date adapter

--- src/main/java/cz/brno/map/utils/DateTimeXmlAdapter.java
+++ src/main/java/cz/brno/map/utils/DateTimeXmlAdapter.java
@@ -1,36 +1,21 @@
 package cz.brno.map.utils;
 
-import javax.annotation.Nonnull;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalQuery;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Created by strukov on 6/30/16.
  */
-public class DateTimeXmlAdapter <T extends TemporalAccessor> extends XmlAdapter<String, T> {
+public class DateTimeXmlAdapter extends XmlAdapter<String, Date> {
 
-    private final DateTimeFormatter formatter;
-    private final TemporalQuery<? extends T> temporalQuery;
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    public DateTimeXmlAdapter(@Nonnull DateTimeFormatter formatter,
-                                      @Nonnull TemporalQuery<? extends T> temporalQuery) {
-        this.formatter = requireNonNull(formatter, "formatter must not be null");
-        this.temporalQuery = requireNonNull(temporalQuery, "temporal query must not be null");
+    public Date unmarshal(final String xml) throws Exception {
+        return this.dateFormat.parse(xml);
     }
 
-    @Override
-    public T unmarshal(String stringValue) {
-        return stringValue != null ? formatter.parse(stringValue, temporalQuery) : null;
-    }
-
-    @Override
-    public String marshal(T value) {
-        return value != null ? formatter.format(value) : null;
+    public String marshal(final Date object) throws Exception {
+        return this.dateFormat.format(object);
     }
 }
