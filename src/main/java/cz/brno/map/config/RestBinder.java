14292ed85be874a2aee03542ae77f71fd4a5a1a8
vadimstrukov <vadim.strukov@gmail.com>
2016-07-01 14:17:13 +0200

    Implemented abstract RestBinder for Date

---
+++ src/main/java/cz/brno/map/config/RestBinder.java
@@ -0,0 +1,21 @@
+package cz.brno.map.config;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by strukov on 7/1/16.
+ */
+public abstract class RestBinder {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+    }
+}
