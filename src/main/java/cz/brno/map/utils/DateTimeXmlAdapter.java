600bf2f92ad611bb14e47ce25796b5bb01b2a7da
vadimstrukov <vadim.strukov@gmail.com>
2016-07-01 09:25:39 +0200

    Fixed DateTimeAdapter. Added timezone

--- src/main/java/cz/brno/map/utils/DateTimeXmlAdapter.java
+++ src/main/java/cz/brno/map/utils/DateTimeXmlAdapter.java
@@ -3,15 +3,17 @@ package cz.brno.map.utils;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by strukov on 6/30/16.
  */
 public class DateTimeXmlAdapter extends XmlAdapter<String, Date> {
 
-    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public Date unmarshal(final String xml) throws Exception {
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
         return this.dateFormat.parse(xml);
     }
 
